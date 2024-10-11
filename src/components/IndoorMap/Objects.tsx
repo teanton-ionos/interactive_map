741ea87a6f39927f3ff7d50fe8278f337ab1eb91
Teodor Anton <teodor-lucian.anton@ionos.com>
2024-10-11 15:01:06 +0300

    Mapped all rooms

--- src/components/IndoorMap/Objects.tsx
+++ src/components/IndoorMap/Objects.tsx
@@ -23,6 +23,42 @@ function Objects({ handleObjectClick, className }: ObjectsProps) {
         d="M567.5 305.5V333.25H556.5V305.5H567.5Z"
         onClick={handleObjectClick}
       />
+      <path
+        id="14.6, 14.7"
+        className={`${className} object`}
+        d="M161.383 188.713L161.304 188.775L161.24 188.85L135.338 219.151L58.4244 163.793L78.2546 138.159L108.152 107.763L135.294 88.3758L193.105 163.824L161.383 188.713Z"
+        onClick={handleObjectClick}
+      />
+      <path
+        id="14.1"
+        className={`${className} object`}
+        d="M633.604 188.142L633.541 188.065L633.464 188.002L602.716 162.632L656.723 84.7644L682.7 104.144L713.612 133.507L733.471 160.306L659.043 219.425L633.604 188.142Z"
+        onClick={handleObjectClick}
+      />
+      <path
+        id="14.2"
+        className={`${className} object`}
+        d="M633.117 453.287L633.196 453.225L633.26 453.15L659.162 422.849L736.076 478.207L716.245 503.841L686.348 534.237L659.206 553.624L601.395 478.176L633.117 453.287Z"
+        onClick={handleObjectClick}
+      />
+      <path
+        id="14.3"
+        className={`${className} object`}
+        d="M566.348 498.88L598.746 480.299L656.551 554.27L634.488 569.139L611.407 581.161L566.348 498.88Z"
+        onClick={handleObjectClick}
+      />
+      <path
+        id="14.4"
+        className={`${className} object`}
+        d="M585.095 595.08L558.606 603.749L526.765 517.043L565.543 500.287L608.166 582.103L585.095 595.08Z"
+        onClick={handleObjectClick}
+      />
+      <path
+        id="14.5"
+        className={`${className} object`}
+        d="M82.2382 499.823L61.443 474.191L131.864 423.332L158.262 452.175L158.315 452.232L158.375 452.281L190.118 477.675L136.826 548.568L110.664 527.251L82.2382 499.823Z"
+        onClick={handleObjectClick}
+      />
       <ellipse
         id="Entrance"
         cx={1157.655}
--- src/store/graphData.ts
+++ src/store/graphData.ts
@@ -16,18 +16,30 @@ export interface GraphData {
 }
 export const graphData: GraphData = {
   vertices: [
-
     { id: 'v24', objectName: null, cx: 240, cy: 147 },
     { id: 'v25', objectName: null, cx: 560, cy: 147 },
-    { id: 'v42', objectName: 'South Elevator Entrance', cx: 203.474, cy: 184.225 },
+    {
+      id: 'v42',
+      objectName: 'South Elevator Entrance',
+      cx: 203.474,
+      cy: 184.225,
+    },
     { id: 'v57', objectName: null, cx: 225, cy: 320 },
     { id: 'v58', objectName: null, cx: 150, cy: 320 },
     { id: 'v62', objectName: null, cx: 150, cy: 220 },
-    { id: 'v72', objectName: 'North Elevator Entrance', cx: 433.087, cy: 846.411 },
-
+    {
+      id: 'v72',
+      objectName: 'North Elevator Entrance',
+      cx: 433.087,
+      cy: 846.411,
+    },
+    { id: 'v73', objectName: '14.6, 14.7', cx: 350, cy: 500 },
+    { id: 'v74', objectName: '14.1', cx: 500, cy: 100 },
+    { id: 'v75', objectName: '14.2', cx: 500, cy: 400 },
+    { id: 'v76', objectName: '14.3', cx: 450, cy: 400 },
+    { id: 'v77', objectName: '14.4', cx: 410, cy: 410 },
+    { id: 'v78', objectName: '14.5', cx: 210, cy: 410 },
   ],
 
-  edges: [
-
-  ],
+  edges: [],
 };
